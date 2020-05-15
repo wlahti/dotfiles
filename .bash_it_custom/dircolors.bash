968c8c9ee1954765fafd930abd69a48a5b25cc64
Matthew Sykes <matthew.sykes@gmail.com>
2020-05-15 09:46:13 -0400

    Minor cleanup for dircolors

--- .bash_it_custom/dircolors.bash
+++ .bash_it_custom/dircolors.bash
@@ -2,5 +2,7 @@
 # vi: set ft=sh:
 
 if [[ -e "$HOME/.dir_colors" ]] && [[ -x "$(command -v dircolors)" ]]; then
-    eval "$(dircolors -b ~/.dir_colors)"
+    eval "$(dircolors -b "$HOME/.dir_colors")"
+elif [[ -e "$HOME/.dircolors" ]] && [[ -x "$(command -v dircolors)" ]]; then
+    eval "$(dircolors -b "$HOME/.dircolors")"
 fi
--- .oh-my-zsh-custom/dircolors.zsh
+++ .oh-my-zsh-custom/dircolors.zsh
@@ -1,5 +1,7 @@
 # vi: set ft=zsh:
 
 if [[ -e "$HOME/.dir_colors" ]] && [[ -x "$(command -v dircolors)" ]]; then
-    eval "$(dircolors -b ~/.dir_colors)"
+    eval "$(dircolors -b "$HOME/.dir_colors")"
+elif [[ -e "$HOME/.dircolors" ]] && [[ -x "$(command -v dircolors)" ]]; then
+    eval "$(dircolors -b "$HOME/.dircolors")"
 fi
