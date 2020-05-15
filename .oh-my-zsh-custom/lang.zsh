578612fc1a64798c29baacf59b66a5b3466129cb
Matthew Sykes <matthew.sykes@gmail.com>
2020-05-15 15:24:32 -0400

    Explicitly set LANG if not set

---
+++ .oh-my-zsh-custom/lang.zsh
@@ -0,0 +1,6 @@
+# vi: set ft=zsh:
+
+# set LANG if it hasn't already been set                                                                                   
+if [ -z "$LANG" ]; then                                                                                                    
+    export LANG="en_US.UTF-8"                                                                                              
+fi
