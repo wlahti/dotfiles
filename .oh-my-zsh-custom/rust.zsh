ed78093f23001515ab65e7e4b0b7e31d7d63cdf3
Matthew Sykes <matthew.sykes@gmail.com>
2020-10-09 11:38:52 -0400

    Conditionally set RUST_SRC_PATH
    The path to the rust source code has changed with recent versions. Try
    to discover the correct path and explicitly set RUST_SRC_PATH.

--- .oh-my-zsh-custom/rust.zsh
+++ .oh-my-zsh-custom/rust.zsh
@@ -3,3 +3,13 @@
 if [[ -f "$HOME/.cargo/env" ]]; then
     source "$HOME/.cargo/env"
 fi
+
+if [[ -x "$(command -v rustc)" ]]; then
+    rust_sysroot=$(rustc --print sysroot)
+    if [[ -d "$rust_sysroot/lib/rustlib/src/rust/src" ]]; then
+        export RUST_SRC_PATH="$rust_sysroot/lib/rustlib/src/rust/src"
+    elif [[ -d "$rust_sysroot/lib/rustlib/src/rust/library" ]]; then
+        export RUST_SRC_PATH="$rust_sysroot/lib/rustlib/src/rust/library"
+    fi
+    unset rust_sysroot
+fi
